0c7445e163fa911dcc74cf896e2f761b900f5187
Rahul ray <102358586+mrrahulray@users.noreply.github.com>
2024-07-02 15:40:56 +0530

    Version 2.0

--- password_generator.py
+++ password_generator.py
@@ -1,4 +1,7 @@
 #password generator
+
+#Version 1.0
+
 import random
 alphabets = [
     'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 
@@ -18,16 +21,30 @@ numbers_count =int(input("how many numbers do you want in your password: "))
 symblols_count =int(input("how many simbles do you want in your password: "))
 
 
-password=""
+password_list=[]
+
 
 for i in range(0,latters_count):
-    password+= random.choice(alphabets)
+    if random.randint(0,1)==1:
+        password_list.append((random.choice(alphabets)).lower())
+    else:
+        password_list.append((random.choice(alphabets)))
+
 
 for i in range(0,numbers_count):
-    password+= str(random.choice(numbers))
+    password_list.append(str(random.choice(numbers)))
 
 for i in range(0,symblols_count):
-    password+= random.choice(symbols)
+    password_list.append(random.choice(symbols))
 
-print(password)
+# Shuffle the password list to ensure randomness
+random.shuffle(password_list)
+
+password= ""
+for i in password_list:
+    password+=i
+print(f"The generated password: {password}")
+
+
+
 
